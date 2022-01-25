edbd25c50c556cab443dd67248ad18d993078dff
Scott Dixon <scott.dixon@shopify.com>
2022-01-25 14:26:58 +1000

    StackBlitz test

---
+++ src/App.server.jsx
@@ -0,0 +1,30 @@
+import {ShopifyServerProvider, DefaultRoutes} from '@shopify/hydrogen';
+import {Switch} from 'react-router-dom';
+import {Suspense} from 'react';
+
+import shopifyConfig from '../shopify.config';
+
+import DefaultSeo from './components/DefaultSeo.server';
+import NotFound from './components/NotFound.server';
+import CartProvider from './components/CartProvider.client';
+import LoadingFallback from './components/LoadingFallback';
+
+export default function App({log, pages, ...serverState}) {
+  return (
+    <Suspense fallback={<LoadingFallback />}>
+      <ShopifyServerProvider shopifyConfig={shopifyConfig} {...serverState}>
+        <CartProvider>
+          <DefaultSeo />
+          <Switch>
+            <DefaultRoutes
+              pages={pages}
+              serverState={serverState}
+              log={log}
+              fallback={<NotFound />}
+            />
+          </Switch>
+        </CartProvider>
+      </ShopifyServerProvider>
+    </Suspense>
+  );
+}
